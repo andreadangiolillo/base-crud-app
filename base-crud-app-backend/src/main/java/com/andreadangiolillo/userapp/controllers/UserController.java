34146479c62e98d8ec9ff4595ae761de291e10be
andreadangiolillo <andrea.dangiolillo@gmail.com>
2025-06-28 13:57:37 +0200

    base crud controller

--- base-crud-app-backend/src/main/java/com/andreadangiolillo/userapp/controllers/UserController.java
+++ base-crud-app-backend/src/main/java/com/andreadangiolillo/userapp/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.andreadangiolillo.userapp.controllers;
 import com.andreadangiolillo.userapp.models.User;
 import com.andreadangiolillo.userapp.services.UserService;
 import com.andreadangiolillo.userapp.services.parameters.UserSearchParameters;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,40 +13,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-public class UserController {
+public class UserController extends BaseCrudController<User, Long>{
 
     private final UserService service;
 
     public UserController(UserService service) {
+        super(service);
         this.service = service;
-    }
-
-    @PostMapping("/create")
-    public ResponseEntity<User> create(@RequestBody User user) {
-        User saved = service.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
-    }
-
-    @GetMapping("/findAll")
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok(service.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.findById(id).orElse(null));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
-        User updated = service.update(id, user);
-        return ResponseEntity.ok(updated);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        service.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/search")
