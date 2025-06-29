42950b8abd5cb6e4860e76038ecfab98a077695b
andreadangiolillo <andrea.dangiolillo@gmail.com>
2025-06-29 11:55:02 +0200

    config app docker

--- base-crud-app-frontend/src/app/app.routes.ts
+++ base-crud-app-frontend/src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./features/home/pages/home/home.component').then(m => m.HomeComponent)
-  },
-  {
-    path: 'users',
-    loadChildren: () =>
-      import('./features/users/users.routes').then(m => m.USER_ROUTES),
-  }
+    {
+        path: '',
+        loadComponent: () =>
+            import('./features/home/pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'users',
+        loadChildren: () =>
+            import('./features/users/users.routes').then(m => m.USER_ROUTES),
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
 ];
